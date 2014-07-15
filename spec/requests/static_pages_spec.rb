6a20bcd7c874ac02225576a47adda1f36977fb91
samarth <samarth@CSDTP-0064.(none)>
2014-07-15 16:48:04 +0530

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the h1 'Books Demo'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Books Demo')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Books Demo | Home")
+    end
+  end
+end
