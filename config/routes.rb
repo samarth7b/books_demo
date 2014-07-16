60280d825890f5f3a06850ee4aeb1e5c8e187a15
samarth <samarth@CSDTP-0064.(none)>
2014-07-16 12:17:41 +0530

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 BooksDemo::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   #get "users/new"
 
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
 
   # The priority is based upon order of creation:
